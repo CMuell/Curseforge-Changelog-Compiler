76c9b31c570aaba59cd152358f29ee0fd02fd4aa
CMuell <camjsmoeller@gmail.com>
2018-02-06 01:39:58 -0800

    configuration changes
    Added: Config file for keys and paths
    Added: Module requirement file for script to check for & download
    dependencies

--- README.md
+++ README.md
@@ -10,22 +10,22 @@ This script scrapes changelogs from curseforge for any jars that are different i
 1. Follow the instructions [on this page](https://developers.google.com/api-client-library/python/guide/aaa_apikeys) to get your google python api key
 
 ### Step 3: Configuring the Script
-There are 5 variables to be configured in changelog.py.
+There are 5 parameters to be configured in 'config.txt'.
 
-**api_key** = the API key you retrieved in step 2 (string)
+**api_key** = the API key you retrieved in step 2
 
-**cse_id** = the CSE ID you retrieved in step 1 (string)
+**cse_id** = the CSE ID you retrieved in step 1
 
-**path_old** = the path to the directory containing the old modpack version mods. (string)
+**path_old** = the path to the directory containing the old modpack version mods.
 
 Eg: *C:\USER\NAME\documents\Curse\Minecraft\Instances\MODPACK_NAME\mods*
 
-**path_new** = the path to the directory containing the new modpack version mods. (string)
+**path_new** = the path to the directory containing the new modpack version mods.
 
-**modpack_name** = The modpack name and current (new) version. (string)
+**modpack_name** = The modpack name and current (new) version.
 
 Eg: *FTB Revelations 1.4.0*
 
-### Once all steps are complete, run Compile_Changelog.bat.
+### Once all steps are complete, start run.bat.
 
 #### The changelog will appear as changelog.html. I will add tag stripping options in the future. 
--- changelog.py
+++ changelog.py
@@ -1,11 +1,28 @@
 # Written by https://www.reddit.com/user/CJDAM/
 # Written in Python 3
+import pip
+from backports import configparser
+
+reqParse = configparser.RawConfigParser()
+reqFilePath = r'required_modules.txt'
+reqParse.read(reqFilePath)
+
+
+def install(package):
+    pip.main(['install', package])
+
+
+for each_section in reqParse.sections():
+    for (each_key, each_val) in reqParse.items(each_section):
+        install(each_val)
 
 import urllib.request
 import bs4
 import re
 from filecmp import dircmp
 from googleapiclient.discovery import build
+import time
+import sys
 
 
 def google_search(term, api, cse, **kwargs):
@@ -13,73 +30,87 @@ def google_search(term, api, cse, **kwargs):
     result = service.cse().list(q=term, cx=cse, **kwargs).execute()
     return result['items']
 
-# Create a Google Custom Search Engine here FIRST => https://cse.google.com/
-# IMPORTANT!!! MUST SET 'SITES TO SEARCH' TO https://minecraft.curseforge.com/ OR THIS WILL NOT WORK
 
-# GET GOOGLE PYTHON API KEY HERE => https://developers.google.com/api-client-library/python/guide/aaa_apikeys
+# Parses data from 'config.txt'
+configParser = configparser.RawConfigParser()
+configFilePath = r'config.txt'
+configParser.read(configFilePath)
 
+# Retrieves script parameters from configparser
+api_key = configParser.get('keys', 'api_key')
+cse_id = configParser.get('keys', 'cse_id')
+path_old = configParser.get('mod_paths', 'path_old_ver')
+path_new = configParser.get('mod_paths', 'path_new_ver')
+modpack_name = configParser.get('misc', 'modpack_name')
+errors = []
 
-# Google customsearch API key
-api_key = ''
-# Google customsearch ID (cx)
-cse_id = ''
-# Old modpack mods directory
-path_old = ''
-# New modpack mods directory
-path_new = ''
-# Eg => FTB Revelations - 1.4.0
-modpack_name = ''
+if api_key == 'API_KEY':
+    errors.append('api_key')
+if cse_id == 'CSE_ID':
+    errors.append('cse_id')
+if path_old == 'OLD_DIR':
+    errors.append('path_old')
+if path_new == 'NEW_DIR':
+    errors.append('path_new')
 
-# Creates the changelog.html file and writes the changelog title
-new_file = open('changelog.html', 'w')
-new_file.write('<h1>{0} Changelog</h1>\n'.format(modpack_name))
-new_file.write('<p>Written by /u/CJDAM<p>')
-new_file.write('<br><br><br>')
-new_file.close()
+if errors.__len__() > 0:
+    print('Please configure the following in config.txt:')
+    for error in errors:
+        print('- {0}'.format(error));
+    time.sleep(5)
+    sys.exit()
 
-# Compares the two mod directories and returns unique files from the new version
-dcmp = dircmp(path_old, path_new)
-right_only = dcmp.right_only
-# Separates the returned file names into individual strings
-mod_array = str(right_only).split(',')
+else:
+    # Creates the changelog.html file and writes the changelog title
+    new_file = open('changelog.html', 'w')
+    new_file.write('<h1>{0} Changelog</h1>\n'.format(modpack_name))
+    new_file.write('<p>Written by /u/CJDAM<p>')
+    new_file.write('<br><br><br>')
+    new_file.close()
 
-print('Working on it...\n')
+    # Compares the two mod directories and returns unique files from the new version
+    dcmp = dircmp(path_old, path_new)
+    right_only = dcmp.right_only
+    # Separates the returned file names into individual strings
+    mod_array = str(right_only).split(',')
 
-# For each string in the mod_array list do the following:
-for name in mod_array:
-    # Defines search_term as the current file name
-    search_term = name
+    print('Working on it...\n')
 
-    print('Getting ' + name + '\n')
+    # For each string in the mod_array list do the following:
+    for name in mod_array:
+        # Defines search_term as the current file name
+        search_term = name
 
-    # Initializes a new google search using the file name, api_key, cse_id. Returns first search result
-    new_search = google_search(name, api_key, cse_id, num=1)
+        print('Getting ' + name + '\n')
 
-    for search in new_search:
-        # Retrieves the curseforge file link from the query
-        res = search['link']
-        # Opens the link and views the html
-        response = urllib.request.urlopen(res)
-        response_html = response.read()
-        soup = bs4.BeautifulSoup(response_html, 'html.parser')
-        # Searches for the 'logbox' class containing changelog information
-        logbox = soup.find('div', class_='logbox')
-        # Converts the retrieved information to a string
-        logbox = str(logbox)
-        # Some prettifying
-        logbox = re.sub('<h1>', '<h4>', logbox)
-        logbox = re.sub('</h1>', '</h4>', logbox)
-        logbox = re.sub('<h2>', '<h3>', logbox)
-        logbox = re.sub('</h2>', '<h3>', logbox)
+        # Initializes a new google search using the file name, api_key, cse_id. Returns first search result
+        new_search = google_search(name, api_key, cse_id, num=1)
 
-        content = logbox
+        for search in new_search:
+            # Retrieves the curseforge file link from the query
+            res = search['link']
+            # Opens the link and views the html
+            response = urllib.request.urlopen(res)
+            response_html = response.read()
+            soup = bs4.BeautifulSoup(response_html, 'html.parser')
+            # Searches for the 'logbox' class containing changelog information
+            logbox = soup.find('div', class_='logbox')
+            # Converts the retrieved information to a string
+            logbox = str(logbox)
+            # Some prettifying
+            logbox = re.sub('<h1>', '<h4>', logbox)
+            logbox = re.sub('</h1>', '</h4>', logbox)
+            logbox = re.sub('<h2>', '<h3>', logbox)
+            logbox = re.sub('</h2>', '<h3>', logbox)
 
-        # Opens changelog.html to append the retrieved changelog to EOF
-        file_object = open('changelog.html', 'a')
-        # Writes file name to changelog.html
-        file_object.write('<div>')
-        file_object.write('<h2>{0}</h2>\n'.format(name))
-        file_object.write('<p>{0}</p>\n'.format(content))
-        file_object.write('</div>')
-        file_object.write('<br><br>')
-        file_object.close()
+            content = logbox
+
+            # Opens changelog.html to append the retrieved changelog to EOF
+            file_object = open('changelog.html', 'a')
+            # Writes file name to changelog.html
+            file_object.write('<div>')
+            file_object.write('<h2>{0}</h2>\n'.format(name))
+            file_object.write('<p>{0}</p>\n'.format(content))
+            file_object.write('</div>')
+            file_object.write('<br><br>')
+            file_object.close()
